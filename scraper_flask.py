fec6e9cf38eb24ca1da8c9331c93a21114c4fd98
h-pham0 <hanhminhpham01@gmail.com>
2024-03-29 01:34:24 -0400

    Remove token

--- scraper_flask.py
+++ scraper_flask.py
@@ -2,18 +2,18 @@ from flask import Flask, jsonify, request
 import requests
 
 app = Flask(__name__)
-TMBD_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTI2YmY1ZDRkMTIwMDAyM2NjN2JiMTcyZTQ3OWU0MSIsInN1YiI6IjY2MDYzNzA0MzAzYzg1MDE3Y2I4NzU3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kB3AeH4Gm5gLdkTyEOEeCZdckS8onu0gHc6tkzbplxI"
+TMDB_TOKEN = 'replace-with-token'
 
 @app.route('/movies', methods=['GET'])
 def get_movies():
     url = "https://api.themoviedb.org/3/account/21151538"
     headers = {
     "accept": "application/json",
-    "Authorization": f"Bearer {TMBD_TOKEN}"
+    "Authorization": f"Bearer {TMDB_TOKEN}"
     }
 
     response = requests.get(url, headers=headers)
     return jsonify(response.json())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
